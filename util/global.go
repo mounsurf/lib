bd74e12c54ec5fe82e4763dd464eb6c1ab07b460
Your Name <you@example.com>
2021-07-09 14:49:11 +0800

    修改IPv4

--- util/global.go
+++ util/global.go
@@ -3,6 +3,7 @@ package util
 import "regexp"
 
 var (
-	IpRegex       = regexp.MustCompile(`^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$`)
+	Ipv4Regex     = regexp.MustCompile(`^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$`)
+	IpRegex       = Ipv4Regex
 	IpSubnetRegex = regexp.MustCompile(`^(\d{1,3}\.){3}\d{1,3}/\d{1,2}$`)
 )
